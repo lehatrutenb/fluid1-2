702f12540171b2b305f85fa96645d782fe7de651
Алексей Маренков <lehatrutenb@gmail.com>
2024-12-18 21:04:39 +0300

    Now it builds, not works

--- fluid.cpp
+++ fluid.cpp
@@ -13,6 +13,7 @@ constexpr size_t N = 36, M = 84; // размеры поля ? TODO
 // constexpr size_t N = 14, M = 5;
 constexpr size_t T = 1'000'000; // кол-во тиков которые будут сделаны
 constexpr std::array<pair<int, int>, 4> deltas{{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}}; // куда из каждой точки попытаемся потечь?
+constexpr long double eps = 1e-6;
 
 // char field[N][M + 1] = {
 //     "#####",
@@ -70,6 +71,8 @@ char field[N][M + 1] = { // само поле M + 1 хз почему
     "####################################################################################",
 };
 
+mt19937 rnd(1303);
+
 template<std::size_t N, std::size_t K, bool F>
 struct Fixed {
     using T = std::conditional<N <= 8,  int8_t, typename std::conditional<N <= 16,  int16_t, typename std::conditional<N <= 32,  int32_t, int64_t>::type>::type>::type;
@@ -80,8 +83,10 @@ struct Fixed {
     template<std::size_t No, std::size_t Ko, bool Fo>
     constexpr Fixed(Fixed<No, Ko, Fo> o): v(o.to_double()*(1<<K)) {}
     constexpr Fixed(): v(0) {}
+    //operator double() const { return to_double(); }
+    //operator float() const { return to_double(); }
 
-    constexpr double to_double() {
+    constexpr double to_double() const {
         return v / (double) (1 << K);
     }
 
@@ -91,9 +96,6 @@ struct Fixed {
         return ret;
     }
     T v;
-
-    auto operator<=>(const Fixed&) const = default;
-    bool operator==(const Fixed&) const = default;
 
     //static constexpr Fixed inf = Fixed::from_raw(std::numeric_limits<T>::max());
     //static constexpr Fixed eps = Fixed::from_raw(deltas.size());
@@ -136,12 +138,66 @@ struct Fixed {
 };
 
 template<std::size_t N, std::size_t K, bool F>
+auto operator<=>(const Fixed<N,K,F>& a, const double& b) {
+    if (a == b) {
+        return 0;
+    } else {
+        return a.to_double() > b ? 1 : -1;
+    }
+}
+
+template<std::size_t N, std::size_t K, bool F>
+bool operator==(const Fixed<N,K,F>& a, const double& b) {
+    return abs(a.to_double() - b) < eps;
+}
+
+template<std::size_t N, std::size_t K, bool F, std::size_t No, std::size_t Ko, bool Fo>
+auto operator<=>(const Fixed<N,K,F>& a, const Fixed<No,Ko,Fo>& b) {
+    if (a == b) {
+        return 0;
+    } else {
+        return a.to_double() > b.to_double() ? 1 : -1;
+    }
+}
+
+template<std::size_t N, std::size_t K, bool F, std::size_t No, std::size_t Ko, bool Fo>
+bool operator==(const Fixed<N,K,F>& a, const Fixed<No,Ko,Fo>& b) {
+    return abs(a.to_double() - b.to_double()) < eps;
+}
+
+template<std::size_t N, std::size_t K, bool F>
 Fixed<N,K,F> abs(Fixed<N,K,F> f) {
     if (f.v < 0) {
         f.v = -f.v;
     }
     return f;
 }
+
+template<typename TO, typename FR>
+TO tryConv(FR x) {
+    return TO(tryConv<double, FR> (x));
+}
+
+template<std::size_t N, std::size_t K, bool F>
+double tryConv(Fixed<N,K,F> x) {
+    return x.to_double();
+}
+
+template<std::size_t N, std::size_t K, bool F>
+Fixed<N,K,F> tryConv(double x) {
+    return Fixed<N,K,F>(x);
+}
+
+template<std::size_t N, std::size_t K, bool F>
+Fixed<N,K,F> tryConv(float x) {
+    return Fixed<N,K,F>(x);
+}
+
+template<std::size_t N, std::size_t K, bool F, std::size_t No, std::size_t Ko, bool Fo>
+Fixed<N,K,F> tryConv(Fixed<No,Ko,Fo> x) {
+    return Fixed<N,K,F>(x);
+}
+
 
 template<std::size_t N, std::size_t K, bool F>
 double operator+(double o, Fixed<N,K,F> t) {
@@ -158,6 +214,7 @@ double operator*(double o, Fixed<N,K,F> t) {
     return o * t.to_double();
 }
 
+
 template<std::size_t N, std::size_t K, bool F>
 double operator/(double o, Fixed<N,K,F> t) {
     return o / t.to_double();
@@ -165,6 +222,11 @@ double operator/(double o, Fixed<N,K,F> t) {
 
 template<std::size_t N, std::size_t K, bool F>
 double& operator+=(double& o, Fixed<N,K,F> t) {
+    return o = t.to_double() + o;
+}
+
+template<std::size_t N, std::size_t K, bool F>
+float& operator+=(float& o, Fixed<N,K,F> t) {
     return o = t.to_double() + o;
 }
 
@@ -179,7 +241,7 @@ double& operator*=(double o, Fixed<N,K,F> t) {
 }
 
 template<std::size_t N, std::size_t K, bool F>
-double operator/(double& o, Fixed<N,K,F> t) {
+double operator/=(double& o, Fixed<N,K,F> t) {
     return o = o / t.to_double();
 }
 
@@ -190,46 +252,45 @@ std::ostream& operator<<(std::ostream &out, const Fixed<N,K,F>& f) {
 
 
 
-template<typename T1, typename T2, typename T3>
+template<typename PT, typename VT, typename VFT>
 struct fluidEmulator {
 
-Fixed rho[256]; // какие-то константы? TODO
+VT rho[256]; // какие-то константы? TODO
 
-Fixed p[N][M]{}, old_p[N][M]; // ? TODO
+PT p[N][M]{}, old_p[N][M]; // ? TODO
 
-struct VectorField {
-    array<Fixed, deltas.size()> v[N][M]; // TODO вероятно для каждой клетки какой-то поток в разные стороны
-    Fixed &add(int x, int y, int dx, int dy, Fixed dv) {
+template<typename T>
+struct VectorField { // changed Fixed->T
+    array<T, deltas.size()> v[N][M]; // TODO вероятно для каждой клетки какой-то поток в разные стороны
+    T &add(int x, int y, int dx, int dy, T dv) {
         return get(x, y, dx, dy) += dv;
     }
 
-    Fixed &get(int x, int y, int dx, int dy) {
+    T &get(int x, int y, int dx, int dy) {
         size_t i = ranges::find(deltas, pair(dx, dy)) - deltas.begin();
         assert(i < deltas.size());
         return v[x][y][i];
     }
 };
 
-VectorField velocity{}, velocity_flow{}; // TODO ?
+VectorField<VT> velocity{};
+VectorField<VFT> velocity_flow{}; // TODO ?
 int last_use[N][M]{}; // базовый used для dfs
 int UT = 0;  // видимо переменная для dfs (или не проверял что там) чтобы не запускаться из одной вершины много раз
 
-
-mt19937 rnd(1337);
-
-tuple<Fixed, bool, pair<int, int>> propagate_flow(int x, int y, Fixed lim) { // координаты рассм клетки и lim TODO?
+tuple<VFT, bool, pair<int, int>> propagate_flow(int x, int y, VFT lim) { // координаты рассм клетки и lim TODO?
     last_use[x][y] = UT - 1; // помечаем клетку посещённой
-    Fixed ret = 0;
+    VFT ret = 0;
     for (auto [dx, dy] : deltas) { // перебираем соседние клетки к нашей
         int nx = x + dx, ny = y + dy; // коорд соседних клеток
         if (field[nx][ny] != '#' && last_use[nx][ny] < UT) { // если рассм клетка не препятствие и не была помечена раньше то идём внутрь
-            auto cap = velocity.get(x, y, dx, dy);
-            auto flow = velocity_flow.get(x, y, dx, dy);
+            VT cap = velocity.get(x, y, dx, dy);
+            VFT flow = velocity_flow.get(x, y, dx, dy);
             if (flow == cap) { // TODO?
                 continue;
             }
             // assert(v >= velocity_flow.get(x, y, dx, dy));
-            auto vp = min(lim, cap - flow); // TODO?
+            VFT vp = lim < (cap - flow) ? (VFT) lim : tryConv<VFT, VT>(cap-flow); // TODO?
             if (last_use[nx][ny] == UT - 1) { // видимо если была подсчитама в пред разы то пересчёт простой TODO?
                 velocity_flow.add(x, y, dx, dy, vp); // добавляем к вытеканию сколько вытечет в напр dx dy TODDO?
                 last_use[x][y] = UT; // помечаем изначальную клетку рассмотренной
@@ -250,8 +311,8 @@ tuple<Fixed, bool, pair<int, int>> propagate_flow(int x, int y, Fixed lim) { // 
     return {ret, 0, {0, 0}}; // TODO?
 }
 
-Fixed random01() {
-    return Fixed::from_raw((rnd() & ((1 << 16) - 1)));
+double random01() {
+    return (rnd() & ((1 << 16) - 1));
 }
 
 // прикол что помечает какие-то клетки рассмотренным, но какие TODO ? 
@@ -260,7 +321,7 @@ void propagate_stop(int x, int y, bool force = false) { // коорд текущ
         bool stop = true;
         for (auto [dx, dy] : deltas) { // перебираем соседей
             int nx = x + dx, ny = y + dy;
-            if (field[nx][ny] != '#' && last_use[nx][ny] < UT - 1 && velocity.get(x, y, dx, dy) > 0) { // если есть хотя бы одна клетка из которой ещё не всё вылилось (пересчиталось) что должно TODO ?
+            if (field[nx][ny] != '#' && last_use[nx][ny] < UT - 1 && velocity.get(x, y, dx, dy) > VT(0)) { // если есть хотя бы одна клетка из которой ещё не всё вылилось (пересчиталось) что должно TODO ?
                 stop = false; // типо все рассмотрели - говорим что можно остановиться
                 break;
             }
@@ -272,15 +333,15 @@ void propagate_stop(int x, int y, bool force = false) { // коорд текущ
     last_use[x][y] = UT; // помечаем клетку рассмотренной если <= velocity.get(x, y, dx, dy) или это бордюрчик или уже рассмотренна на последнем тике
     for (auto [dx, dy] : deltas) { // итерируемся по соседним клеткам
         int nx = x + dx, ny = y + dy;
-        if (field[nx][ny] == '#' || last_use[nx][ny] == UT || velocity.get(x, y, dx, dy) > 0) { // если бордюрчик/уже помечена/ещё не всё вытекло ?TODO - не рассматриваем
+        if (field[nx][ny] == '#' || last_use[nx][ny] == UT || velocity.get(x, y, dx, dy) > VT(0)) { // если бордюрчик/уже помечена/ещё не всё вытекло ?TODO - не рассматриваем
             continue;
         }
         propagate_stop(nx, ny); // рекурсивно смотрим соседнюю
     }
 }
 
-Fixed move_prob(int x, int y) { // координаты клетки
-    Fixed sum = 0;
+VT move_prob(int x, int y) { // координаты клетки
+    VT sum = 0;
     for (size_t i = 0; i < deltas.size(); ++i) { // перебираем соседей
         auto [dx, dy] = deltas[i];
         int nx = x + dx, ny = y + dy;
@@ -288,7 +349,7 @@ Fixed move_prob(int x, int y) { // координаты клетки
             continue;
         }
         auto v = velocity.get(x, y, dx, dy); // смотрим сколько чего-то а ней TODO?
-        if (v < 0) {
+        if (v < VT(0)) {
             continue;
         }
         sum += v; // суммируем чего-то в клетке, если положительное число TODO ?
@@ -298,10 +359,10 @@ Fixed move_prob(int x, int y) { // координаты клетки
 
 struct ParticleParams { // класс чтобы инициализировать/менять клетки поля TODO?
     char type;
-    Fixed cur_p;
-    array<Fixed, deltas.size()> v;
+    PT cur_p;
+    array<VT, deltas.size()> v;
 
-    void swap_with(int x, int y) {
+    void swap_with(PT p[N][M], VectorField<VT>& velocity, int x, int y) {
         swap(field[x][y], type);
         swap(p[x][y], cur_p);
         swap(velocity.v[x][y], v);
@@ -313,8 +374,8 @@ bool propagate_move(int x, int y, bool is_first) { // клетки поля и i
     bool ret = false;
     int nx = -1, ny = -1;
     do {
-        std::array<Fixed, deltas.size()> tres;
-        Fixed sum = 0;
+        std::array<VT, deltas.size()> tres;
+        VT sum = 0;
         for (size_t i = 0; i < deltas.size(); ++i) { // перебираем соседей
             auto [dx, dy] = deltas[i];
             int nx = x + dx, ny = y + dy;
@@ -323,7 +384,7 @@ bool propagate_move(int x, int y, bool is_first) { // клетки поля и i
                 continue;
             }
             auto v = velocity.get(x, y, dx, dy);
-            if (v < 0) {
+            if (v < VT(0)) {
                 tres[i] = sum;
                 continue;
             }
@@ -331,17 +392,17 @@ bool propagate_move(int x, int y, bool is_first) { // клетки поля и i
             tres[i] = sum;
         }
 
-        if (sum == 0) {
+        if (sum == VT(0)) {
             break;
         }
 
-        Fixed p = random01() * sum;
+        double p = random01() * sum;
         size_t d = std::ranges::upper_bound(tres, p) - tres.begin();
 
         auto [dx, dy] = deltas[d];
         nx = x + dx;
         ny = y + dy;
-        assert(velocity.get(x, y, dx, dy) > 0 && field[nx][ny] != '#' && last_use[nx][ny] < UT);
+        assert(velocity.get(x, y, dx, dy) > VT(0) && field[nx][ny] != '#' && last_use[nx][ny] < UT);
 
         ret = (last_use[nx][ny] == UT - 1 || propagate_move(nx, ny, false));
     } while (!ret);
@@ -349,27 +410,27 @@ bool propagate_move(int x, int y, bool is_first) { // клетки поля и i
     for (size_t i = 0; i < deltas.size(); ++i) { // перебираем соседей
         auto [dx, dy] = deltas[i];
         int nx = x + dx, ny = y + dy;
-        if (field[nx][ny] != '#' && last_use[nx][ny] < UT - 1 && velocity.get(x, y, dx, dy) < 0) { // если не бордюр, не была ещё рассмотренная и что-то в ней есть TODO?
+        if (field[nx][ny] != '#' && last_use[nx][ny] < UT - 1 && velocity.get(x, y, dx, dy) < VT(0)) { // если не бордюр, не была ещё рассмотренная и что-то в ней есть TODO?
             propagate_stop(nx, ny);
         }
     }
     if (ret) { // TODO?
         if (!is_first) { // TODO?
             ParticleParams pp{}; // TODO?
-            pp.swap_with(x, y);
-            pp.swap_with(nx, ny);
-            pp.swap_with(x, y);
+            pp.swap_with(p, velocity, x, y);
+            pp.swap_with(p, velocity, nx, ny);
+            pp.swap_with(p, velocity, x, y);
         }
     }
     return ret;
 }
 
-int dirs[N][M]{}; // TODO?
+double dirs[N][M]{}; // TODO?
 
 void run() {
     rho[' '] = 0.01; // задаём константы
     rho['.'] = 1000;
-    Fixed g = 0.1; // типо g физическая
+    VT g = 0.1; // типо g физическая
 
     for (size_t x = 0; x < N; ++x) {
         for (size_t y = 0; y < M; ++y) {
@@ -383,7 +444,7 @@ void run() {
 
     for (size_t i = 0; i < T; ++i) { // итерируемся по тикам
         
-        Fixed total_delta_p = 0;
+        PT total_delta_p = 0;
         // Apply external forces
         for (size_t x = 0; x < N; ++x) {
             for (size_t y = 0; y < M; ++y) {
@@ -403,18 +464,18 @@ void run() {
                 for (auto [dx, dy] : deltas) {
                     int nx = x + dx, ny = y + dy;
                     if (field[nx][ny] != '#' && old_p[nx][ny] < old_p[x][y]) {
-                        auto delta_p = old_p[x][y] - old_p[nx][ny];
-                        auto force = delta_p;
-                        auto &contr = velocity.get(nx, ny, -dx, -dy);
+                        PT delta_p = old_p[x][y] - old_p[nx][ny];
+                        PT force = delta_p;
+                        VT &contr = velocity.get(nx, ny, -dx, -dy);
                         if (contr * rho[(int) field[nx][ny]] >= force) {
                             contr -= force / rho[(int) field[nx][ny]];
                             continue;
                         }
                         force -= contr * rho[(int) field[nx][ny]];
                         contr = 0;
-                        velocity.add(x, y, dx, dy, force / rho[(int) field[x][y]]);
-                        p[x][y] -= force / dirs[x][y];
-                        total_delta_p -= force / dirs[x][y];
+                        velocity.add(x, y, dx, dy, tryConv<VT, PT>((force) / rho[(int) field[x][y]]));
+                        p[x][y] -= force / VT(dirs[x][y]);
+                        total_delta_p -= force / VT(dirs[x][y]);
                     }
                 }
             }
@@ -430,7 +491,7 @@ void run() {
                 for (size_t y = 0; y < M; ++y) {
                     if (field[x][y] != '#' && last_use[x][y] != UT) {
                         auto [t, local_prop, _] = propagate_flow(x, y, 1);
-                        if (t > 0) {
+                        if (t > VFT(0)) {
                             prop = 1;
                         }
                     }
@@ -446,18 +507,18 @@ void run() {
                 for (auto [dx, dy] : deltas) {
                     auto old_v = velocity.get(x, y, dx, dy);
                     auto new_v = velocity_flow.get(x, y, dx, dy);
-                    if (old_v > 0) {
+                    if (old_v > VT(0)) {
                         assert(new_v <= old_v);
-                        velocity.get(x, y, dx, dy) = new_v;
+                        velocity.get(x, y, dx, dy) = tryConv<VT, VFT>(new_v);
                         auto force = (old_v - new_v) * rho[(int) field[x][y]];
                         if (field[x][y] == '.')
                             force *= 0.8;
                         if (field[x + dx][y + dy] == '#') {
-                            p[x][y] += force / dirs[x][y];
-                            total_delta_p += force / dirs[x][y];
+                            p[x][y] += force / PT(dirs[x][y]);
+                            total_delta_p += force / PT(dirs[x][y]);
                         } else {
-                            p[x + dx][y + dy] += force / dirs[x + dx][y + dy];
-                            total_delta_p += force / dirs[x + dx][y + dy];
+                            p[x + dx][y + dy] += force / PT(dirs[x + dx][y + dy]);
+                            total_delta_p += force / PT(dirs[x + dx][y + dy]);
                         }
                     }
                 }
@@ -546,7 +607,7 @@ int main() {
     typeSetter::setType(first_type_b, first_type, [&]<typename T1>() {
         typeSetter::setType(second_type_b, second_type, [&]<typename T2>() {
             typeSetter::setType(third_type_b, third_type, [&]<typename T3>() {
-                run<T1, T2,T3>();
+                fluidEmulator<T1, T2,T3>{}.run();
             });
         });
     });
